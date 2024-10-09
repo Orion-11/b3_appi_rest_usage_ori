f82387c2d83d3cce94e0aa788b392a830603869e
orion <orilizgon@gmail.com>
2024-10-09 16:14:42 -0500

    ok

--- src/components/ApicoffeeCarruselBasic.jsx
+++ src/components/ApicoffeeCarruselBasic.jsx
@@ -1,6 +1,31 @@
 
+import axios from 'axios'; 
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 export const ApicoffeeCarruselBasic = () => {
-  return (
+
+ const [cofeeList, setCoffeeList]= useState([]);
+
+ useEffect(() => {
+
+    const fetchData = async () => {
+        try {
+
+            const response = await axios.get('https://api.sampleapis.com/coffee/hot');
+            setCoffeeList(response.data);
+            console.log(cofeeList);
+
+        } catch (error) {
+            console.log('Error al recuperar los datos de la API', error);
+        }
+    };
+    
+    fetchData();
+
+ }, []);
+
+     return (
     <div>ApicoffeeCarruselBasic</div>
-  )
-}
+  );
+};
